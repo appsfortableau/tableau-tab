4fe5adf642105bd71c8d99688841f7410ee9e7b8
Yoram de Langen <yoram.de.langen@infotopics.nl>
2019-11-22 20:23:34 +0100

    Added page config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,22 @@
 module.exports = {
   publicPath: "./",
   lintOnSave: true,
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/index/main.js',
+      // the source template
+      template: 'public/index.html',
+      // output as dist/index.html
+      filename: 'index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "Tableau Tab | AppsForTableau",
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    }
+  },
   devServer: {
     headers: {
       "X-Frame-Options": "sameorigin"
